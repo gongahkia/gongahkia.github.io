3f2c5a8946f0f3558533e6c1e74f516c2279518b
gongahkia <angryapplegravy@gmail.com>
2025-02-24 04:19:19 +0800

    feat(app): aded

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# seventh version of personal site
+# ninth version of personal site
 
 THIS SITE'S SOURCE CODE CAN BE FOUND [HERE](https://github.com/gongahkia/gabrielong.com)!
 
@@ -8,8 +8,3 @@ THIS SITE'S SOURCE CODE CAN BE FOUND [HERE](https://github.com/gongahkia/gabriel
 ![HTML](https://img.shields.io/badge/-HTML-000?&logo=html5)
 ![CSS](https://img.shields.io/badge/-CSS-000?&logo=css3)
 ![Github](https://img.shields.io/badge/-Github%20Sites-000?&logo=github)
-
-### resources
-
-* [icons](https://simpleicons.org/?q=smile)
-* [visual reference](https://hajimehoshi.com/)
--- script.js
+++ script.js
@@ -1,4 +1,45 @@
-// ----- SETUP CODE -----
+"use strict";
+
+// --- actual running code ---
+
+const theButton = document.getElementById("infinityButton");
+let isDarkMode = false; 
+
+theButton?.addEventListener("click", pressTheButton);
+
+function pressTheButton() {
+    isDarkMode = !isDarkMode; 
+    const newColor = isDarkMode ? generateDarkColor() : generateLightColor();
+    console.log(newColor, isDarkMode); 
+    document.body.style.backgroundColor = newColor;
+    const articleTag = document.getElementsByClassName("overallArticleTags")[0];
+    const imageTag = document.getElementById("gongImage");
+    if (isDarkMode) {
+        theButton.setAttribute("style", "filter:invert(1);");
+        articleTag.setAttribute("style", "filter:invert(1);");
+        imageTag.style.filter = "invert(1)";
+    } else {
+        theButton.style.filter = "none";
+        articleTag.style.filter = "none";
+        imageTag.style.filter = "none";
+    }
+}
+
+function generateDarkColor() {
+    const r = Math.floor(Math.random() * 128);
+    const g = Math.floor(Math.random() * 128);
+    const b = Math.floor(Math.random() * 128);
+    return `rgb(${r}, ${g}, ${b})`;
+}
+
+function generateLightColor() {
+    const r = Math.floor(Math.random() * 128) + 128;
+    const g = Math.floor(Math.random() * 128) + 128;
+    const b = Math.floor(Math.random() * 128) + 128;
+    return `rgb(${r}, ${g}, ${b})`;
+}
+
+// ----- setup code -----
 
 const config = {
     timeZone: 'Asia/Singapore',
@@ -8,81 +49,9 @@ const config = {
 },
 formatter = new Intl.DateTimeFormat([], config);
 
+// ----- execution code for current time -----
+
 const currentYear = new Date().getFullYear();
-
-// ----- ENCRYPTION -----
-
-function a(b) {
-    return Array.from(new Uint8Array(b)).map(b => b.toString(16).padStart(2, '0')).join('');
-}
-
-function c(d) {
-    let e = "";
-    for (let a of d) {
-        e += a.charCodeAt(0).toString();
-    }
-    return e;
-}
-
-function e(a) {
-    const c = Math.ceil(Math.sqrt(a.length)); 
-    const d = [];
-    for (let e = 0; e < c; e++) {
-        d[e] = [];
-        for (let f = 0; f < c; f++) {
-            const g = e * c + f;
-            if (g < Math.sqrt(a.length)) {
-                d[e][f] = a[g];
-            } else {
-                d[e][f] = ''; 
-            }
-        }
-    }
-    let h = '';
-    for (let f = 0; f < c; f++) {
-        for (let e = 0; e < c; e++) {
-            if (d[e][f]) { 
-                h += d[e][f];
-            }
-        }
-    }
-    return h;
-}
-
-function g(a, b) {
-    const c = a.split('').reverse().join('');
-    const d = b.length;
-    const e = c.slice(-d) + c.slice(0, -d);
-    const f = b + e + b;
-    return f;
-}
-
-function h(a, b) {
-    b = b.length;
-    let d = "";
-    for (let e = 0; e < a.length; e++) {
-        let f = a.charCodeAt(e);
-        if (f >= 65 && f <= 90) { 
-            d += String.fromCharCode(((f - 65 + b) % 26) + 65);
-        } else if (f >= 97 && f <= 122) { 
-            d += String.fromCharCode(((f - 97 + b) % 26) + 97);
-        } else {
-            d += a[e]; 
-        }
-    }
-    return d;
-}
-
-function i(j) {
-    return j + Date.now(); 
-}
-
-async function f(t) {
-    const e = new TextEncoder();
-    return a(await window.crypto.subtle.digest('SHA-256', e.encode(t)));
-}
-
-// ----- EXECUTION CODE -----
 
 setInterval(
     () => {
@@ -91,17 +60,3 @@ setInterval(
 , 1000)
 
 document.querySelector("#current-year").innerText = currentYear;
-
-const t = "If you decrypt this, send me the salt on LinkedIn.";
-const s = "gongahkia";
-(async () => document.querySelector("#secret").innerText = await f(i(h(g(e(c(s + t)), s), s))))();
-
-// ----- PREVENTION CODE -----
-
-document.addEventListener("contextmenu", event => event.preventDefault());
-document.addEventListener("keydown", event => {
-    if (event.key === "F12" || (event.ctrlKey && event.shiftKey && event.key === "I") || (event.ctrlKey && event.shiftKey && event.key === "J") || (event.ctrlKey && event.shiftKey && event.key === "C") || (event.ctrlKey && event.shiftKey && event.key === "K")) {
-        event.preventDefault();
-        // window.location.href = "about:blank";
-    }
-});
